886080938af86e454f7cab590d5da75de30c1864
Ismoil <ismoilkabulov2@gmail.com>
2022-03-20 18:46:19 +0300

    Js start

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var x = (prompt("Ismingizni kiriting... "))
+console.log('Sizning ismingiz ' +  x)
+var x = (prompt('Yoshingiz nechida?'))
+console.log('Man  ' + x  +' Yoshdaman')
+var x =  y = (prompt('Qaysi soha vakilisiz? '))
+console.log('Hozirda ' + x + ' bolib ish yuritmoqdaman')
+var x = (prompt('Nima uchun bu sohani tanlagansiz?'))
+console.log(x +' Toxtovsiz harakat erishilgan maqsad bosurin brodar');
+var x = (prompt('Hoyotdagi shioringixz nima?'))
+console.log(x + ' Shunaqa');
+var x = (prompt('Oldingizga qoygan eng kotta maqsadingiz(niyatingiz)?'))
+console.log('In SHa Alloh ' + x)
+var x = (prompt ('Misolni yeching 128-66=? '))
+console.log('Sizning javobingiz ' + x +' Togri javob 62')
+var x =(prompt ('77-8=? '))
+console.log('Sizning javobingiz ' + x + ' Togri javob 69');
+
+alert("Javoblarigiz uchun spasiba hayr salomat boling.(Javoblaringizni korish uchun konsolni oching.)")
+
