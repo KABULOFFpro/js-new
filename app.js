2f5ab8edae2e44cb03e3c2987261aeb574eefe08
Ismoil <ismoilkabulov2@gmail.com>
2022-03-23 13:03:02 +0300

    Madal oyna

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var x =  y = (prompt('Qaysi soha vakilisiz? '))
 console.log('Hozirda ' + x + ' bolib ish yuritmoqdaman')
 var x = (prompt('Nima uchun bu sohani tanlagansiz?'))
 console.log(x +' Toxtovsiz harakat erishilgan maqsad bosurin brodar');
-var x = (prompt('Hoyotdagi shioringixz nima?'))
+var x = (prompt('Hoyotdagi shioringiz nima?'))
 console.log(x + ' Shunaqa');
 var x = (prompt('Oldingizga qoygan eng kotta maqsadingiz(niyatingiz)?'))
 console.log('In SHa Alloh ' + x)
@@ -15,6 +15,8 @@ var x = (prompt ('Misolni yeching 128-66=? '))
 console.log('Sizning javobingiz ' + x +' Togri javob 62')
 var x =(prompt ('77-8=? '))
 console.log('Sizning javobingiz ' + x + ' Togri javob 69');
+var x = (prompt('6 + 3 = ?'))
+console.log('Misol 1: 12 + 3 = 15 Sizning javob ' + x)
 
 alert("Javoblarigiz uchun spasiba hayr salomat boling.(Javoblaringizni korish uchun konsolni oching.)")
 
